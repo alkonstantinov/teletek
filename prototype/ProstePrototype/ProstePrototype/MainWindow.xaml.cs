aafde58d9dbb7a7479ec8c4682b5d478beb97acb
Vikadie <68245263+Vikadie@users.noreply.github.com>
2022-07-07 22:24:28 +0300

    location / nacigation added

--- prototype/ProstePrototype/ProstePrototype/MainWindow.xaml.cs
+++ prototype/ProstePrototype/ProstePrototype/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace ProstePrototype
             //Uri iconUri = new Uri("pack://application:,,,/html/Icon.ico", UriKind.RelativeOrAbsolute);
             //this.Icon = BitmapFrame.Create(iconUri);
             string applicationDirectory = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            //string navigation = System.IO.Path.Combine(applicationDirectory, "html", "nav.html");
+            string firstFile = System.IO.Path.Combine(applicationDirectory, "html", "index.html");
             string myFile = System.IO.Path.Combine(applicationDirectory, "html", "index.html");
             //string myFile = System.IO.Path.Combine(applicationDirectory, "html", "access.html");
             //string myFile = System.IO.Path.Combine(applicationDirectory, "html", "panels_in_network.html");
@@ -39,7 +41,17 @@ namespace ProstePrototype
             //string myFile = System.IO.Path.Combine(applicationDirectory, "html", "peripherial_devices.html");
             //string myFile = System.IO.Path.Combine(applicationDirectory, "html", "peripherial_devices_none.html");
 
-            wb1.Load("file:///" + myFile);
+            //wb0.Load("file:///" + navigation);
+            this.DataContext = this;
+            wb1.Load("file:///" + firstFile);
+            if (myFile == firstFile)
+            {
+                wb1.Width = 0; //if index.html is loaded
+                Splitter1.Width = 0; //if index.html is loaded
+            } else
+            {
+                wb1.Width = this.Width / 5;
+            }
             wb2.Load("file:///" + myFile);
         }
 
@@ -58,11 +70,47 @@ namespace ProstePrototype
         //    Environment.Exit(0);
         //}
 
+        private void ChangeTheme_Click(object sender, RoutedEventArgs e)
+        {
+            string script = @"$().click(()=>{
+                $(["".light [class*='-light']"", "".dark [class*='-dark']""]).each((i, ele) => {
+                    $(ele).toggleClass('bg-light bg-dark')
+                    $(ele).toggleClass('text-light text-dark')
+                    $(ele).toggleClass('navbar-light navbar-dark')
+                })
+                    // toggle body class selector
+                    $('body').toggleClass('light dark')
+                })";
+            wb2.ExecuteScriptAsync(script);
+        }
+
+        private void wb_JSBreadCrumb(object sender, JavascriptMessageReceivedEventArgs e)
+        {
+            var i = ((string)e.Message).Split('|');
+            string str = i[0];
+            for (int j = 1; j < i.Length; j++)
+            {
+                str += " \\ " + i[j];
+            }
+            this.Dispatcher.Invoke(() =>
+            {
+                if (i.Length > 1)
+                {
+                    if (i.Length > 2)
+                    {
+                        string wb1CommandLine = "div" + i[i.Length - 2];
+                        wb1.ExecuteScriptAsync("loadDiv(" + wb1CommandLine +", " + i[0] + "|" + i[i.Length - 2]+ ")"); // not working
+                    }
+                    wb1.Width = this.Width / 5;
+                    Splitter1.Width = 5;
+                }
+                NavField.Text = str;// your code here.
+            });
+        }
         private void Read_Clicked(object sender, RoutedEventArgs e)
         {
             string script = "$('#divModal').modal('show')";
             wb2.ExecuteScriptAsync(script);
-
         }
         private void Exit_Clicked(object sender, RoutedEventArgs e)
         {
