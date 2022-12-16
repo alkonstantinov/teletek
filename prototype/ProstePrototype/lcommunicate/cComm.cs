1f8d43a900f24622a284446e3364c5de05ec7c49
Pavel Popov <pavel.popov.ol@outlook.com>
2022-12-16 15:07:01 +0200

    List elements and value

--- prototype/ProstePrototype/lcommunicate/cComm.cs
+++ prototype/ProstePrototype/lcommunicate/cComm.cs
@@ -20,6 +20,8 @@ namespace lcommunicate
     internal class cTransport
     {
         internal virtual object Connect(object o) { return null; }
+        internal virtual void Close(object o) { }
+        internal virtual byte[] SendCommand(object _connection, byte[] _command) { return null; }
     }
     public class cComm
     {
@@ -27,6 +29,11 @@ namespace lcommunicate
         /// Dictionary с ключ ID на панел и съдържание Dictionary<~path, value>
         /// </summary>
         private static Dictionary<string, Dictionary<string, string>> _cache_panels;
+        /// <summary>
+        /// Dictionary с ключ ID на панел и съдържание Dictionary<~path, Dictionary с шаблони>.
+        /// Dictionary с шаблони е с ключ индекса и елемент Groups за съответния елемент с преправени пътища с добавен индекс.
+        /// </summary>
+        private static Dictionary<string, Dictionary<string, Dictionary<string, string>>> _cache_list_panels;
         private static object _cs_cache = new object();
 
         public static void SetPathValue(string panel_id, string path, string value)
@@ -46,20 +53,64 @@ namespace lcommunicate
 
         public static string GetPathValue(string panel_id, string path)
         {
+            string res = null;
             Monitor.Enter(_cs_cache);
-            if (!_cache_panels.ContainsKey(panel_id))
-                return null;
-            Dictionary<string, string> panel = _cache_panels[panel_id];
-            if (!panel.ContainsKey(path))
-                return null;
-            string val = panel[path];
+            if (_cache_panels != null && _cache_panels.ContainsKey(panel_id))
+            {
+                Dictionary<string, string> panel = _cache_panels[panel_id];
+                if (panel != null && panel.ContainsKey(path))
+                    res = panel[path];
+            }
             Monitor.Exit(_cs_cache);
-            return val;
+            return res;
+        }
+
+        /// <summary>
+        /// Добавя в кеша шаблон на списъчен елемент(панел в мрежата, вход, изход...).
+        /// В последствие този шаблон се ползва за съхраняване на стойности за съответния елемент.
+        /// </summary>
+        /// <param name="panel_id">ID на панела
+        /// <param name="key">ID на елемента(напр. "IRIS8_PANELINNETWORK")</param>
+        /// <param name="idx">Индекс, на който се добавя елемента</param>
+        /// <param name="_template">Шаблон на елемента. Пътищата в него трябва да се преправят</param>
+        public static void AddListElement(string panel_id, string key, string idx, string _template)
+        {
+            Monitor.Enter(_cs_cache);
+            if (_cache_list_panels == null)
+                _cache_list_panels = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
+            if (!_cache_list_panels.ContainsKey(panel_id))
+                _cache_list_panels.Add(panel_id, new Dictionary<string, Dictionary<string, string>>());
+            Dictionary<string, Dictionary<string, string>> el = _cache_list_panels[panel_id];
+            if (!el.ContainsKey(key))
+                el.Add(key, new Dictionary<string, string>());
+            Dictionary<string, string> lst = el[key];
+            if (!lst.ContainsKey(idx))
+                lst.Add(idx, _template);
+            Monitor.Exit(_cs_cache);
+        }
+
+        public static string GetListElement(string panel_id, string key, string idx)
+        {
+            string res = null;
+            Monitor.Enter(_cs_cache);
+            if (_cache_list_panels.ContainsKey(panel_id))
+            {
+                Dictionary<string, Dictionary<string, string>> el = _cache_list_panels[panel_id];
+                if (el.ContainsKey(key))
+                {
+                    Dictionary<string, string> lst = el[key];
+                    if (lst.ContainsKey(idx))
+                        res = lst[idx];
+                }
+            }
+            Monitor.Exit(_cs_cache);
+            return (res != null) ? res : "{}";
         }
         public static JArray Scan()
         {
             cTransport t = new cIP();
-            object conn = t.Connect(new cIPParams("212.36.21.86", 7000));
+            //object conn = t.Connect(new cIPParams("92.247.2.162", 7000));
+            //t.Close(conn);
             return JArray.Parse("[{ deviceType: 'fire', schema: 'iris8', title: 'IRIS', interface: 'IP', address: '212.36.21.86:7000'}, { deviceType: '', schema: 'eclipse99', title: 'ECLIPSE', interface: 'COM', address: 'COM1'}]");
         }
     }
--- prototype/ProstePrototype/lcommunicate/cIP.cs
+++ prototype/ProstePrototype/lcommunicate/cIP.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net.Sockets;
 using common;
+using System.IO;
 
 namespace lcommunicate
 {
     internal class cIP : cTransport
     {
+        private byte[] _ver_cmd = new byte[] { 0x07, 0x51, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        private byte[] _cmd = new byte[] { 0x03, 0x51, 0x0F, 0x00, 0x00, 0x89 };
+        private byte[] _time_cmd = new byte[] { 0x00, 0x23, 0xFF };
+        //
+        //
+        private cIPParams _last_ip = null;
         internal override object Connect(object o)
         {
             cIPParams ip = (cIPParams)o;
-            //TcpClient c = new TcpClient(ip.address, ip.port);
-            return null;
+            TcpClient c = new TcpClient(ip.address, ip.port);
+            _last_ip = ip;
+            byte[] ver = SendCommand(c, _time_cmd);
+            return c;
+        }
+
+        internal override void Close(object o)
+        {
+            ((TcpClient)o).Close();
+        }
+
+        internal override byte[] SendCommand(object _connection, byte[] _command)
+        {
+            TcpClient _conn = (TcpClient)_connection;
+            if (!_conn.Connected)
+                if (_last_ip != null)
+                    _conn.Connect(_last_ip.address, _last_ip.port);
+                else return base.SendCommand(_conn, _command);
+            //
+            NetworkStream netstr = _conn.GetStream();
+            StreamReader r = new StreamReader(netstr);
+            netstr.Write(_command, 0, _command.Length);
+            string res = r.ReadToEnd();
+            //
+            return base.SendCommand(_conn, _command);
         }
     }
 }
